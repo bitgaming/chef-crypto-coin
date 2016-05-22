8bea1d68df0f1b04fda7887363d7ba7dfef15424
Ed Bosher <ed@bitgamelabs.com>
2016-05-22 19:22:34 +0800

    needs lib event dev now

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "andrew@bedesign.be"
 license          "MIT"
 description      "Installs/Configures crypto-coin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.9.5"
+version          "0.9.6"
 
 depends          "apt"
 
